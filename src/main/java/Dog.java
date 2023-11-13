65a11bef4bf93c3b3bc28839786090211da3f168
MuLangballe <mala0007@stud.kea.dk>
2023-11-13 13:16:10 +0100

    eat metode

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -3,4 +3,11 @@ public class Dog extends Animal{
     public void makeSound() {
         System.out.println("vov!");
     }
+
+    @Override
+    public boolean eat(String foodType) {
+        return true;
+    }
+
+
 }
